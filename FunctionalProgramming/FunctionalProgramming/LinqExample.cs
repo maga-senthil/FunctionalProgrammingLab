b73a34713153631091cc7e4703276b38f8016b4c
maga-senthil <magalakshmi.s@gmail.com>
2016-09-09 14:28:34 -0500

    initial

---
+++ FunctionalProgramming/FunctionalProgramming/LinqExample.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{ 
+    class LinqExample
+    {
+        string value ="10,20,30,40";
+
+        string str = "greatJobwhat";
+        public void CalculateAverage()
+        {
+            int i;
+            var a = value.Split(new[] { ',' }).Select(s => int.TryParse(s, out i) ? i : 0).ToArray();
+
+            double average = a.Average(mark => mark);
+            Console.WriteLine(average);
+
+        }
+
+        public void AlphabeticallyCounted()
+        { 
+            string sortString = String.Concat(str.ToUpper().OrderBy(c => c));
+
+            var counts = sortString.GroupBy(c => c).Select(g => new { Char = g.Key, Count = g.Count() });
+
+            foreach (var c in counts)
+            {
+                Console.Write("{0}{1}", c.Char, c.Count);
+            }
+
+        }
+    }
+}
---
+++ FunctionalProgramming/FunctionalProgramming/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinqExample linqExample = new LinqExample();
+            linqExample.CalculateAverage();
+            linqExample.AlphabeticallyCounted();
+            Console.ReadLine();
+        }
+    }
+}
